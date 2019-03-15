32cda2072c029e88f1f3ed1e2c5fe99f8686cc4a
Vladislav <xxxigrik@mail.ru>
2019-03-15 16:46:29 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+const (
+	api_url = "https://vm-pd-nifi-1.dh.rt.ru:8080/nifi-api"
+)
+
+type app struct {
+	Token string
+}
+
+type client struct {
+	client *http.Client
+}
+
+func (c *client) makeClient() {
+	// Create New http Transport
+	transCfg := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // disable verify
+	}
+	// Create Http Client
+	c.client = &http.Client{Transport: transCfg}
+}
+
+func main() {
+
+	a := app{}
+
+	c := client{}
+	c.makeClient()
+
+	form := url.Values{}
+	form.Set("username", "vladislav.mirkos")
+	form.Set("password", "Gfhjkmxxxigrik1234")
+
+	req, err := http.NewRequest("POST", "https://vm-pd-nifi-1.dh.rt.ru:8080/nifi-api/access/token", strings.NewReader(form.Encode()))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	// Check Error
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	a.Token = string(data)
+	fmt.Println(a.Token)
+	//------------------
+	sum := 0
+	for i := 0; i < 1000; i++ {
+		sum += i
+	}
+	//--------------------------------------------------
+	req1, err1 := http.NewRequest("GET", "https://vm-pd-nifi-1.dh.rt.ru:8080/nifi-api/process-groups/518182e7-0168-1000-ffff-ffff8966214b", nil)
+	if err1 != nil {
+		log.Fatalln(err)
+	}
+	req1.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
+
+	resp1, err2 := c.client.Do(req1)
+	if err2 != nil {
+		log.Fatalln(err)
+	}
+
+	data1, err3 := ioutil.ReadAll(resp1.Body)
+	// Check Error
+	if err3 != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(data1))
+
+	defer resp1.Body.Close()
+
+}
---
+++ types.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"go/types"
+)
+
+type RevisionDTO struct {
+	ClientId     string `json:"client_id"`
+	Version      int    `json:"version"`
+	LastModifier string `json:"last_modifier"`
+}
+
+type PositionDTO struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+}
+
+type Position struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+}
+
+type AccessConfigurationDTO struct {
+	SupportsLogin bool `json:"supports_login"`
+}
+
+type AccessStatusDTO struct {
+	Identity string `json:"identity"`
+	Status   string `json:"status"`
+	Message  string `json:"message"`
+}
+
+type AccessStatusEntity struct {
+	AccessStatus AccessStatusDTO `json:"access_status"`
+}
+
+type PermissionsDTO struct {
+	CanRead  bool `json:"can_read"`
+	CanWrite bool `json:"can_write"`
+}
+
+type BulletinDTO struct {
+}
+
+type BulletinEntity struct {
+	Id          int64       `json:"id"`
+	GroupId     string      `json:"group_id"`
+	SourceId    string      `json:"source_id"`
+	Timestamp   string      `json:"timestamp"`
+	NodeAddress string      `json:"node_address"`
+	CanRead     bool        `json:"can_read"`
+	Bulletin    BulletinDTO `json:"bulletin"`
+}
+
+type ProcessGroupDTO struct {
+	Id                           string                       `json:"id"`
+	VersionedComponentId         string                       `json:"versioned_component_id"`
+	ParentGroupId                string                       `json:"parent_group_id"`
+	Position                     PositionDTO                  `json:"position"`
+	Name                         string                       `json:"name"`
+	Comments                     string                       `json:"comments"`
+	Variables                    types.Object                 `json:"variables"`
+	VersionControlInformation    VersionControlInformationDTO `json:"version_control_information"`
+	RunningCount                 int32                        `json:"running_count"`
+	StoppedCount                 int32                        `json:"stopped_count"`
+	InvalidCount                 int32                        `json:"invalid_count"`
+	DisabledCount                int32                        `json:"disabled_count"`
+	InactiveRemotePortCount      int32                        `json:"inactive_remote_port_count"`
+	UpToDateCount                int32                        `json:"up_to_date_count"`
+	LocallyModifiedCount         int32                        `json:"locally_modified_count"`
+	StaleCount                   int32                        `json:"stale_count"`
+	LocallyModifiedAndStaleCount int32                        `json:"locally_modified_and_stale_count"`
+	SyncFailureCount             int32                        `json:"sync_failure_count"`
+	InputPortCount               int32                        `json:"input_port_count"`
+	OutputPortCount              int32                        `json:"output_port_count"`
+	Contents                     FlowSnippetDTO               `json:"contents"`
+}
+
+type FlowSnippetDTO struct {
+}
+
+type VersionControlInformationDTO struct {
+}
+
+type ProcessGroupStatusDTO struct {
+	Id                 string                              `json:"id"`
+	Name               string                              `json:"name"`
+	StatsLastRefreshed string                              `json:"stats_last_refreshed"`
+	AggregateSnapshot  ProcessGroupStatusSnapshotDTO       `json:"aggregate_snapshot"`
+	NodeSnapshots      []NodeProcessGroupStatusSnapshotDTO `json:"node_snapshots"`
+}
+
+type Link struct {
+	Type       string       `json:"type"`
+	Rels       []string     `json:"rels"`
+	Rel        string       `json:"rel"`
+	Uri        string       `json:"uri"`
+	Params     types.Object `json:"params"`
+	Title      string       `json:"title"`
+	UriBuilder UriBuilder   `json:"uri_builder"`
+}
+
+type UriBuilder struct{}
+
+type VersionedFlowSnapshotMetadata struct {
+	Link             Link   `json:"link"`
+	BucketIdentifier string `json:"bucket_identifier"`
+	FlowIdentifier   string `json:"flow_identifier"`
+	Version          int32  `json:"version"`
+	Timestamp        int64  `json:"timestamp"`
+	Author           string `json:"author"`
+	Comments         string `json:"comments"`
+}
+
+type VersionedRemoteProcessGroup struct {
+	Identifier            string        `json:"identifier"`
+	Name                  string        `json:"name"`
+	Comments              string        `json:"comments"`
+	Position              Position      `json:"position"`
+	TargetUri             string        `json:"target_uri"`
+	TargetUris            string        `json:"target_uris"`
+	CommunicationsTimeout string        `json:"communications_timeout"`
+	YieldDuration         string        `json:"yield_duration"`
+	TransportProtocol     string        `json:"transport_protocol"`
+	LocalNetworkInterface string        `json:"local_network_interface"`
+	ProxyHost             string        `json:"proxy_host"`
+	ProxyPort             int32         `json:"proxy_port"`
+	ProxyUser             string        `json:"proxy_user"`
+	InputPorts            []InputPorts  `json:"input_ports"`
+	OutputPorts           []OutputPorts `json:"output_ports"`
+	ComponentType         string        `json:"component_type"`
+	GroupIdentifier       string        `json:"group_identifier"`
+}
+
+type VersionedProcessor struct {
+	Identifier                       string       `json:"identifier"`
+	Name                             string       `json:"name"`
+	Comments                         string       `json:"comments"`
+	Position                         Position     `json:"position"`
+	Bundle                           Bundle       `json:"bundle"`
+	Style                            types.Object `json:"style"`
+	Type                             string       `json:"type"`
+	Properties                       types.Object `json:"properties"`
+	PropertyDescriptors              types.Object `json:"property_descriptors"`
+	AnnotationData                   string       `json:"annotation_data"`
+	SchedulingPeriod                 string       `json:"scheduling_period"`
+	SchedulingStrategy               string       `json:"scheduling_strategy"`
+	ExecutionNode                    string       `json:"execution_node"`
+	PenaltyDuration                  string       `json:"penalty_duration"`
+	YieldDuration                    string       `json:"yield_duration"`
+	BulletinLevel                    string       `json:"bulletin_level"`
+	RunDurationMillis                int64        `json:"run_duration_millis"`
+	ConcurrentlySchedulableTaskCount int64        `json:"concurrently_schedulable_task_count"`
+	AutoTerminatedRelationships      []string     `json:"auto_terminated_relationships"`
+	ComponentType                    string       `json:"component_type"`
+	GroupIdentifier                  string       `json:"group_identifier"`
+}
+
+type VersionedPort struct {
+	Identifier                       string   `json:"identifier"`
+	Name                             string   `json:"name"`
+	Comments                         string   `json:"comments"`
+	Position                         Position `json:"position"`
+	Type                             string   `json:"type"`
+	ConcurrentlySchedulableTaskCount int32    `json:"concurrently_schedulable_task_count"`
+	ComponentType                    string   `json:"component_type"`
+	GroupIdentifier                  string   `json:"group_identifier"`
+}
+
+type VersionedProcessGroup struct {
+	Identifier               string                        `json:"identifier"`
+	Name                     string                        `json:"name"`
+	Comments                 string                        `json:"comments"`
+	Position                 Position                      `json:"position"`
+	ProcessGroups            []VersionedProcessGroup       `json:"process_groups"`
+	RemoteProcessGroups      []VersionedRemoteProcessGroup `json:"remote_process_groups"`
+	Processors               []VersionedProcessor          `json:"processors"`
+	InputPorts               []VersionedPort               `json:"input_ports"`
+	OutputPorts              []VersionedPort               `json:"output_ports"`
+	Connections              []VersionedConnection         `json:"connections"`
+	Labels                   []VersionedLabel              `json:"labels"`
+	Funnels                  []VersionedFunnel             `json:"funnels"`
+	ControllerServices       []VersionedControllerService  `json:"controller_services"`
+	VersionedFlowCoordinates VersionedFlowCoordinates      `json:"versioned_flow_coordinates"`
+	Variables                types.Object                  `json:"variables"`
+	ComponentType            string                        `json:"component_type"`
+	GroupIdentifier          string                        `json:"group_identifier"`
+}
+
+type Permissions struct {
+	CanRead   bool `json:"can_read"`
+	CanWrite  bool `json:"can_write"`
+	CanDelete bool `json:"can_delete"`
+}
+
+type VersionedFlow struct {
+	Link              Link        `json:"link"`
+	Identifier        string      `json:"identifier"`
+	Name              string      `json:"name"`
+	Description       string      `json:"description"`
+	BucketIdentifier  string      `json:"bucket_identifier"`
+	BucketName        string      `json:"bucket_name"`
+	CreatedTimestamp  int64       `json:"created_timestamp"`
+	ModifiedTimestamp int64       `json:"modified_timestamp"`
+	Type              string      `json:"type"`
+	Permissions       Permissions `json:"permissions"`
+	VersionCount      int64       `json:"version_count"`
+}
+
+type Bucket struct {
+	Link             Link        `json:"link"`
+	Identifier       string      `json:"identifier"`
+	Name             string      `json:"name"`
+	CreatedTimestamp int64       `json:"created_timestamp"`
+	Description      string      `json:"description"`
+	Permissions      Permissions `json:"permissions"`
+}
+
+type NodeProcessGroupStatusSnapshotDTO struct {
+}
+
+type ProcessGroupStatusSnapshotDTO struct {
+}
+
+type versionedFlowSnapshot struct {
+	SnapshotMetadata VersionedFlowSnapshotMetadata `json:"snapshot_metadata"`
+	FlowContents     VersionedProcessGroup         `json:"flow_contents"`
+	Flow             VersionedFlow                 `json:"flow"`
+	Bucket           Bucket                        `json:"bucket"`
+	Latest           bool                          `json:"latest"`
+}
+
+type ProcessGroupEntity struct {
+	Revision                     RevisionDTO           `json:"revision"`
+	Id                           string                `json:"id"`
+	Url                          string                `json:"url"`
+	Position                     PositionDTO           `json:"position"`
+	Permissions                  PermissionsDTO        `json:"permissions"`
+	Bulletins                    []BulletinEntity      `json:"bulletins"`
+	DisconnectedNodeAcknowledged bool                  `json:"disconnected_node_acknowledged"`
+	Component                    ProcessGroupDTO       `json:"component"`
+	Status                       ProcessGroupStatusDTO `json:"status"`
+	VersionedFlowSnapshot        versionedFlowSnapshot `json:"versioned_flow_snapshot"`
+	RunningCount                 int32                 `json:"running_count"`
+	StoppedCount                 int32                 `json:"stopped_count"`
+	InvalidCount                 int32                 `json:"invalid_count"`
+	DisabledCount                int32                 `json:"disabled_count"`
+	ActiveRemotePortCount        int32                 `json:"active_remote_port_count"`
+	InactiveRemotePortCount      int32                 `json:"inactive_remote_port_count"`
+	VersionedFlowState           string                `json:"versioned_flow_state"`
+	UpToDateCount                int32                 `json:"up_to_date_count"`
+	LocallyModifiedCount         int32                 `json:"locally_modified_count"`
+	StaleCount                   int32                 `json:"stale_count"`
+	LocallyModifiedAndStaleCount int32                 `json:"locally_modified_and_stale_count"`
+	SyncFailureCount             int32                 `json:"sync_failure_count"`
+	InputPortCount               int32                 `json:"input_port_count"`
+	OutputPortCount              int32                 `json:"output_port_count"`
+}
